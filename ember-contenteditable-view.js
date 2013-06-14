c1d416ab009348d452f357a8b46169ddb87f45a2
Mehul Kar <mehul.kar@gmail.com>
2013-06-14 02:09:10 -0700

    update html when view is inserted
    If the value is not null when the View is inserted, the view doesn't reflect that. This fixes that bug.

--- ember-contenteditable-view.js
+++ ember-contenteditable-view.js
@@ -1,31 +1,40 @@
 App.ContenteditableView = Em.View.extend({
 	tagName: 'div',
 	attributeBindings: ['contenteditable'],
-	
+
 	// Variables:
 	contenteditable: 'true',
 	isUserTyping: false,
-	
+
 	// Observers:
 	valueObserver: (function() {
 		if (!this.get('isUserTyping') && this.get('value')) {
-			this.$().html(this.get('value'));
+			this._updateHtml();
 		}
 	}).observes('value'),
-	
+
 	// Events:
+	didInsertElement: function() {
+		this._updateHtml();
+	},
+
 	focusOut: function() {
 		this.set('isUserTyping', false);
 	},
-	
+
 	keyDown: function(event) {
 		if (!event.metaKey) {
 			this.set('isUserTyping', true);
 		}
 	},
-	
+
 	keyUp: function(event) {
 		this.set('value', this.$().html());
-	}
+	},
 	
+	_updateHtml: function() {
+		this.$().html(this.get('value'));
+	},
+
+
 });
